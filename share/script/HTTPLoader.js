07bc639ab731af528de90834410c7508f4abf4e5
Evgeniy Aleksandrovich <ckpunmkug@gmail.com>
2025-01-05 14:48:35 +0500

    -

--- share/script/HTTPLoader.js
+++ share/script/HTTPLoader.js
@@ -1,77 +1,84 @@
-// Usage example
-/*{{{
-}}}*/
 
-function HTTPLoader(
+async function HTTPLoader(
+	$method, 
+	$url,
+	$data,
+	$user, $password, // Authorization - Basic
 	$timeout = 30, // in seconds
 	$maxResponseLength = 16 // in megabytes
-) {
-
-	this.load = function($method, $url, $data, $user, $password) {
+	
+	// Return object with property `event`
+	// load - http request complete
+	// abort - when response length more then maxResponseLength
+) {//{{{//
+	
+	var wrapper = function($method, $url, $data, $user, $password, $timeout, $maxResponseLength, resolve, reject) {
+		//console.log($method, $url, $data, $user, $password, $timeout, $maxResponseLength, resolve, reject);
+		
+		$maxResponseLength = 0x100000 * $maxResponseLength;
+		$timeout = 1000 * $timeout;
+		
 		var $XMLHttpRequest = new XMLHttpRequest();
-		$XMLHttpRequest.open($method, $url, true);
-		
+		$XMLHttpRequest.open($method, $url, true);	
 		$XMLHttpRequest.responseType = "text";
-		
-		$XMLHttpRequest.addEventListener("load", this.onLoad.bind(this, $XMLHttpRequest, $url));
-		$XMLHttpRequest.addEventListener("error", this.onError.bind(this, $XMLHttpRequest, $url));
-		
-		$XMLHttpRequest.timeout = this.timeout;
-		$XMLHttpRequest.addEventListener("timeout", this.onTimeout.bind(this, $XMLHttpRequest, $url));
-		
-		$XMLHttpRequest.addEventListener("progress", this.onProgress.bind(this, $XMLHttpRequest, $url));
-		$XMLHttpRequest.addEventListener("abort", this.onAbort.bind(this, $XMLHttpRequest, $url));
-		
+		$XMLHttpRequest.timeout = $timeout;
 		if(typeof($user) == "string" && typeof($password) == "string") {
-			$XMLHttpRequest.setRequestHeader(
-				"Authorization", 
-				" Basic " + btoa($user + ":" + $password)
-			);
+			$XMLHttpRequest.setRequestHeader("Authorization", " Basic "+btoa($user+":"+$password));
 		}
+	
+		var onLoad = function($XMLHttpRequest, $maxResponseLength, resolve, onAbort) {
+			if($XMLHttpRequest.response.length > $maxResponseLength) {
+				$XMLHttpRequest.abort();
+				onAbort(resolve);
+			}
+			$result = {
+				event: 'load'
+				,status: $XMLHttpRequest.status
+				,headers: $XMLHttpRequest.getAllResponseHeaders()
+				,body: $XMLHttpRequest.response
+			};
+			resolve($result);
+		};
+		
+		var onProgress = function($XMLHttpRequest, $maxResponseLength, resolve, onAbort, event) {
+			var $loaded = event.loaded;
+			if($loaded > $maxResponseLength) {
+				$XMLHttpRequest.abort()
+				onAbort(resolve);
+			}
+		};
+		
+		var onAbort = function(resolve) {
+			$result = {
+				event: 'abort'
+			};
+			resolve($result);
+		};
+		
+		var onTimeout = function(resolve) {
+			$result = {
+				event: 'timeout'
+			};
+			resolve($result);
+		};
+		
+		var onError = function(resolve) {
+			$result = {
+				event: 'error'
+			};
+			resolve($result);
+		};
+		
+		$XMLHttpRequest.addEventListener('load', onLoad.bind(null, $XMLHttpRequest, $maxResponseLength, resolve, onAbort));
+		$XMLHttpRequest.addEventListener('progress', onProgress.bind(null, $XMLHttpRequest, $maxResponseLength, resolve, onAbort));
+		$XMLHttpRequest.addEventListener('abort', onAbort.bind(null, resolve));
+		$XMLHttpRequest.addEventListener('timeout', onTimeout.bind(null, resolve));
+		$XMLHttpRequest.addEventListener('error', onError.bind(null, resolve));
 		
 		$XMLHttpRequest.send($data);
 	};
-
-	this.maxResponseLength = 0x100000 * $maxResponseLength;
-
-	this.onLoad = function($XMLHttpRequest, $url) {
-		if($XMLHttpRequest.response.length > this.maxResponseLength) {
-			console.error("Response length exceeds maximum", $url);
-			this.complete($url, -4, '', '');
-			return;
-		}
-		
-		var $status = $XMLHttpRequest.status;
-		var $headers = $XMLHttpRequest.getAllResponseHeaders();
-		var $body = $XMLHttpRequest.response;
-		
-		this.complete($url, $status, $headers, $body);
-	};
+	var $result = new Promise(wrapper.bind(null, $method, $url, $data, $user, $password, $timeout, $maxResponseLength));
+	return($result);
 	
-	this.onError = function($XMLHttpRequest, $url, $event) {
-		console.error("XMLHttpRequest failed", $url);
-		this.complete($url, -1, '', '');
-	};
-
-	this.timeout = 1000 * $timeout;
-	
-	this.onTimeout = function($XMLHttpRequest, $url)	{
-		console.error("XMLHttpRequest timeout", $url);
-		this.complete($url, -2, '', '');
-	};
-	
-	this.onProgress = function($XMLHttpRequest, $url, $event) { 
-		if($event.loaded > this.maxResponseLength) {
-			console.error("Response length exceeds maximum", $url);
-			$XMLHttpRequest.abort();
-		}
-	};
-	
-	this.onAbort = function($XMLHttpRequest, $url) {
-		this.complete($url, -3, '', '');
-	};
-	
-	this.complete = function($url, $status, $headers, $body) {
-	};
-};
+}//}}}//
 
